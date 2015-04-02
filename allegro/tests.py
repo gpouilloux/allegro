7be34a6d3108b49986bc320dc7497e409b77e37f
Guillaume Pouilloux <guillaume.pouilloux@etudiant.mines-nantes.fr>
2015-04-02 11:53:45 +0200

    coverage added

--- allegro/tests.py
+++ allegro/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
 
 # Create your tests here.
+
+
+class SimpleTest(TestCase):
+
+    def test_helloworld(self):
+        client = Client()
+        response = client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
