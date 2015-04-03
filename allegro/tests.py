3f458e802a72f886769a2fce6e2fb5a363924acf
Guillaume Pouilloux <guillaume.pouilloux@etudiant.mines-nantes.fr>
2015-04-03 19:23:08 +0200

    unit test when uploading a text file

--- allegro/tests.py
+++ allegro/tests.py
@@ -1,8 +1,6 @@
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase, Client
 from django.core.urlresolvers import reverse
-
-# Create your tests here.
 from allegro.models import Document
 
 
@@ -20,7 +18,19 @@ class SimpleTest(TestCase):
         doc = Document.objects.get(docfile=docname)
         self.assertEqual(doc.docfile, docname)
 
-    def test_list(self):
+    def test_listget(self):
         client = Client()
         response = client.get(reverse('list'))
         self.assertEqual(response.status_code, 200)
+
+    def test_listpost(self):
+        client = Client()
+        docname = 'toto.txt'
+        docfile = SimpleUploadedFile(docname, "file_content", content_type="text/plain")
+        response = client.post(reverse('list'), {'docfile': docfile})
+        self.assertEqual(response.status_code, 302) # HTTP 302 FOUND
+        try:
+            queryset = Document.objects.get(pk=1)
+            self.assertIsNotNone(queryset.id)
+        except Document.DoesNotExist:
+            self.fail('document should exist')
