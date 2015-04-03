786eecc0589f6a20fb69f07b48a774a0cbc0e455
Guillaume Pouilloux <guillaume.pouilloux@etudiant.mines-nantes.fr>
2015-04-03 17:34:35 +0200

    before_script, ask Travis to syncdb

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='allegrovocal',
+    version='1.0',
+    packages=['allegro', 'allegro.migrations', 'allegrovocal'],
+    url='',
+    license='',
+    author='Guillaume Pouilloux',
+    author_email='gui.pouilloux@gmail.com',
+    description=''
+)
