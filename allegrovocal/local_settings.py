5c11931d6b471597a89c2938cfc010a7e54a3a3d
Guillaume Pouilloux <guillaume.pouilloux@etudiant.mines-nantes.fr>
2015-04-05 20:38:39 +0200

    Split settings between local and production

--- allegrovocal/local_settings.py
+++ allegrovocal/local_settings.py
@@ -15,6 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Build paths inside the project like this: os.path.join(PROJECT_ROOT, ...)
 PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
--- allegrovocal/settings.py
+++ allegrovocal/settings.py
@@ -17,9 +17,9 @@ PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES = {'default': dj_database_url.config()}  # TODO should add default in .config()
-DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
-#DATABASES['default']['NAME'] = 'eph'
+DATABASES = {'default': dj_database_url.config()}
+#DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+#DATABASES['default']['NAME'] = 'postgres'
 
 
 # Quick-start development settings - unsuitable for production
@@ -62,17 +62,6 @@ ROOT_URLCONF = 'allegrovocal.urls'
 
 WSGI_APPLICATION = 'allegrovocal.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-#DATABASES = {
-#    'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#       #  'TEST_NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
